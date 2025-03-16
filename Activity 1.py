3afb1e8a654c127872814c278a48822bfb5a30d3
Prajeeth777 <prajeethkumar777@gmail.com>
2025-03-16 19:54:07 +0900

    Create Activity 1.py

---
+++ Activity 1.py
@@ -0,0 +1,21 @@
+def fun1(n):
+  return n*(n+1)/2
+
+n=int(input("Enter range for natural numbers"))
+
+def func2(n):
+  sum=0
+  for i in range(1,n+1):
+    sum+=i
+  return sum
+
+def func2(n):
+  sum=0
+  for i in range(1,n+1):
+    for j in range(1,i+1)
+      sum+=i
+  return sum
+
+print("Sum of ",n," natural number is ",fun1(n))
+print("Sum of ",n," natural number is ",fun2(n))
+print("Sum of ",n," natural number is ",fun3(n))
