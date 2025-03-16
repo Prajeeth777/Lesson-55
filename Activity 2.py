d7b24fc199bddca32a582e1d966e0ef0ebe8a5db
Prajeeth777 <prajeethkumar777@gmail.com>
2025-03-16 20:08:58 +0900

    Create Activity 2.py

---
+++ Activity 2.py
@@ -0,0 +1,26 @@
+def func1(n):
+  iterations=1
+  return iterations
+
+n=int(input("Enter range for natural numbers"))
+
+def func2(n):
+  iterations=1
+  sum=0
+  for i in range(1,n+1):
+    sum+=i
+    iterations+=1
+  return iterations
+
+def func3(n):
+  iterations=1
+  sum=0
+  for i in range(1,n+1):
+    for j in range(1,i+1:)
+      sum+=i
+      iterations+=1
+  return iterations
+
+print("No of iterations is ",func1(n))
+print("No of iterations is ",func2(n))
+print("No of iterations",func3(n))
